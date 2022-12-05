6757ec4eb3a64126ba3bc8004386f37fea7e5fdc
nikhilacsu <007728624@coyote.csusb.edu>
2022-12-05 11:58:24 -0800

    Food scene update

--- Assets/Scenes/Scripts/CollideEat.cs
+++ Assets/Scenes/Scripts/CollideEat.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -10,6 +11,21 @@ public class CollideEat : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         GetComponentInChildren<Animator>().enabled = true;
-        Destroy(gameObject, 1);     
+        Destroy(gameObject, 1);
+        if (gameObject.name == "Apple" && PlayerPrefs.GetInt("incrementAppleBingo") > 0)
+        {
+            PlayerPrefs.SetInt("incrementAppleBingo", (PlayerPrefs.GetInt("incrementAppleBingo") - 1));
+            GameObject.Find("appleText").GetComponent<TMP_Text>().text = PlayerPrefs.GetInt("incrementAppleBingo").ToString();
+        }
+        else if (gameObject.name == "Strawberry" && PlayerPrefs.GetInt("incrementStrawBingo") > 0)
+        {
+            PlayerPrefs.SetInt("incrementStrawBingo", PlayerPrefs.GetInt("incrementStrawBingo") - 1);
+            GameObject.Find("strawberryText").GetComponent<TMP_Text>().text = PlayerPrefs.GetInt("incrementStrawBingo").ToString();
+        }
+        else if (gameObject.name == "Grapes" && PlayerPrefs.GetInt("incrementGrapesBingo") > 0)
+        {
+            PlayerPrefs.SetInt("incrementGrapesBingo", PlayerPrefs.GetInt("incrementGrapesBingo") - 1);
+            GameObject.Find("grapesText").GetComponent<TMP_Text>().text = PlayerPrefs.GetInt("incrementGrapesBingo").ToString();
+        }
     }
 }
--- Assets/Scenes/Scripts/DragDrop.cs
+++ Assets/Scenes/Scripts/DragDrop.cs
@@ -30,19 +30,5 @@ public class DragDrop : MonoBehaviour,  IBeginDragHandler, IEndDragHandler, IDra
     public void OnEndDrag(PointerEventData eventData)
     {
         m_CanvasGroup.blocksRaycasts = true;
-        if (gameObject.name == "Apple" && PlayerPrefs.GetInt("incrementAppleBingo") > 0)
-        {
-            PlayerPrefs.SetInt("incrementAppleBingo", (PlayerPrefs.GetInt("incrementAppleBingo") - 1));
-            GameObject.Find("appleText").GetComponent<TMP_Text>().text = PlayerPrefs.GetInt("incrementAppleBingo").ToString();
-        }
-        else if (gameObject.name == "Strawberry" && PlayerPrefs.GetInt("incrementStrawBingo") > 0)
-        {
-            PlayerPrefs.SetInt("incrementStrawBingo", PlayerPrefs.GetInt("incrementStrawBingo") - 1);
-            GameObject.Find("strawberryText").GetComponent<TMP_Text>().text = PlayerPrefs.GetInt("incrementStrawBingo").ToString();        }
-        else if (gameObject.name == "Grapes" && PlayerPrefs.GetInt("incrementGrapesBingo") > 0)
-        {
-            PlayerPrefs.SetInt("incrementGrapesBingo", PlayerPrefs.GetInt("incrementGrapesBingo") - 1);
-            GameObject.Find("grapesText").GetComponent<TMP_Text>().text = PlayerPrefs.GetInt("incrementGrapesBingo").ToString();
-        }
     }
 }
